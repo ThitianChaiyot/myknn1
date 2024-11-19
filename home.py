f9501e05ca613677081ccc8a2bdcc64b340c42e6
ThitianChaiyot <155653091+ThitianChaiyot@users.noreply.github.com>
2024-11-19 14:48:48 +0700

    Update home.py

--- home.py
+++ home.py
@@ -4,4 +4,9 @@ import pandas as pd
 st.title("😎😎Website Developing using Python😎😎")
 st.header("🐷🐷Website Developing using Python🐷🐷")
 
+st.image('./img/myp.jpg')
 st.subheader("Thitinan Chaiyot")
+
+dt=pd.read_csv('./data/iris-3.csv')
+st.header()
+st.write(dt.head(10))
