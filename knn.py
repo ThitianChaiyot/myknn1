bbf355eeb3330ea193ca1d3695e7b3535059a4f5
ThitianChaiyot <155653091+ThitianChaiyot@users.noreply.github.com>
2024-11-19 15:44:14 +0700

    Add files via upload

---
+++ knn.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+st.header("โปรเจ็คการจำเเนกข้อมูลดอกไม้")
+st.imae("ใส่รูป")
+
+dt=pd.read_csv('./data/iris-3.csv')
+st.subheader('ข้อมูลดิบของ iris')
+st.write(dt.head(10))
+
+st.subheader('การจำเเนกข้อมูลใหม่')
